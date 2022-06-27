bb832507964301b124aef55eb9b8d2a1f2990153
Shraddhaindhane <106793928+Shraddhaindhane@users.noreply.github.com>
2022-06-27 13:35:27 +0530

    Add files via upload

---
+++ Source2.java
@@ -0,0 +1,56 @@
+package practice;
+
+	class Activity{
+		String string1;
+		String string2;
+		String operator;
+		public Activity(String string1, String string2, String operator) {
+			this.string1 = string1;
+			this.string2 = string2;
+			this.operator = operator;
+		}
+	}
+	public class Source2 {
+		public String handleException(Activity a) {
+			String string;
+			try {
+				if (a.string1.equals(null) || a.string2.equals(null))
+					throw new NullPointerException("Null values found");
+				if (!(a.operator.equals("+")) && !(a.operator.equals("-")))
+					throw new Exception("Default exception"+a.operator);
+			}
+			catch (NullPointerException ex) {
+				string= "Null values found";
+				System.out.println(string);
+				return string;
+			}
+			catch (Exception e) {
+				string= "Default Exception"+a.operator;
+				System.out.println(string);
+				return string;
+			}
+			return "No Exception Found";
+		}
+		public String doOperation(Activity a){
+			//String string = a.operator;
+			String result= new String();
+			switch (a.operator) {
+			case "+":{result=a.string1.concat(a.string2);
+			System.out.println(result);
+			return result;
+			}
+			case "-":{ String regex=a.string2;
+			result=a.string1.replaceAll(regex, "");
+			System.out.println(result);
+			return result;
+			}
+			}
+			return result ;
+		}
+		public static void main(String args[] ) throws Exception {
+			Source2 source=new Source2();
+			Activity activity = new Activity("Helloworld", " world ", "+");
+			source.handleException(activity);
+			source.doOperation(activity);
+		}
+	}
